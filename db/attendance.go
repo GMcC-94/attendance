77cc866c58abc1066d77610285eb173e6af28e01
Dev-VM <gerardmccann94@hotmail.co.uk>
2025-06-18 17:34:03 +0100

    DB: Added query for inserting attendance

--- db/attendance.go
+++ db/attendance.go
@@ -1,1 +1,14 @@
 package db
+
+import (
+	"database/sql"
+	"time"
+)
+
+func InsertAttendance(db *sql.DB, studentID int, date time.Time, dayOfWeek string) error {
+	_, err := db.Exec("INSERT into attendance (student_id, date, day_of_week) VALUES ($1, $2, $2)",
+		studentID,
+		date,
+		dayOfWeek)
+	return err
+}
