2bacc6793c8b28b9a71b70ae34539de2854ba5bd
Dev-VM <gerardmccann94@hotmail.co.uk>
2025-08-04 16:58:27 +0100

    CreateSession func

---
+++ backend/db/sessions.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/gmcc94/attendance-go/rand"
+	"github.com/gmcc94/attendance-go/types"
+)
+
+type PostgresSessionsStore struct {
+	DB            *sql.DB
+	BytesPerToken int
+}
+
+func (p *PostgresSessionsStore) CreateSession(userID int) (*types.Session, error) {
+	bytesPerToken := p.BytesPerToken
+	if bytesPerToken < types.MinBytesPerToken {
+		bytesPerToken = types.MinBytesPerToken
+	}
+
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create session: %w", err)
+	}
+
+	session := types.Session{
+		UserID:    userID,
+		Token:     token,
+		TokenHash: p.hash(token),
+	}
+	row := p.DB.QueryRow(`
+	INSERT INTO sessions (user_id, token_hash)
+	VALUES ($1, $2) ON CONFLIT (user_id) DO
+	UPDATE
+	SET token_hash = $2
+	RETURNING id;`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create session: %w", err)
+	}
+
+	return &session, nil
+}
+
+func (p *PostgresSessionsStore) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
+}
