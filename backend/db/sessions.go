90c8a7f85c8ad1a92909519ea3095b01566fbd72
Dev-VM <gerardmccann94@hotmail.co.uk>
2025-08-06 14:38:56 +0100

    Add Users and Delete func for sessions

--- backend/db/sessions.go
+++ backend/db/sessions.go
@@ -12,6 +12,7 @@ import (
 
 type SessionStore interface {
 	CreateSession(userID int) (*types.Session, error)
+	User(token string) (*types.User, error)
 }
 
 type PostgresSessionsStore struct {
@@ -49,6 +50,36 @@ func (p *PostgresSessionsStore) CreateSession(userID int) (*types.Session, error
 	return &session, nil
 }
 
+func (p *PostgresSessionsStore) User(token string) (*types.User, error) {
+	tokenHash := p.hash(token)
+
+	var user types.User
+	row := p.DB.QueryRow(`
+	SELECT users.id,
+	users.username,
+	users.password_hash
+	FROM sessions
+	JOIN users on users.id = sessions.user_id
+	WHERE sessions.token_hash = $1`, tokenHash)
+	err := row.Scan(&user.ID, &user.Username, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user session: %w", err)
+	}
+	return &user, nil
+}
+
+func (p *PostgresSessionsStore) Delete(token string) error {
+	tokenHash := p.hash(token)
+
+	_, err := p.DB.Exec(`
+	DELETE FROM sessions
+	WHERE token_hash = $1;`, tokenHash)
+	if err != nil {
+		return fmt.Errorf("delete session: %w", err)
+	}
+
+	return nil
+}
 func (p *PostgresSessionsStore) hash(token string) string {
 	tokenHash := sha256.Sum256([]byte(token))
 	return base64.URLEncoding.EncodeToString(tokenHash[:])
